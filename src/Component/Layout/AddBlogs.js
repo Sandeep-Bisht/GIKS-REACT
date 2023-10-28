8d0478e06713280273555592cf14b9661171c524
Sandeep Bisht <>
2023-10-28 15:25:50 +0530

    change blog irl

--- src/Component/Layout/AddBlogs.js
+++ src/Component/Layout/AddBlogs.js
@@ -19,8 +19,8 @@ const AddBlog = () => {
 
   // const navigate=useNavigate();
 
-  // let url = "http://185.239.209.106:4500/api"
-    let url = "http://localhost:4500/api";
+  let url = "http://185.239.209.106:4500/api"
+    // let url = "http://localhost:4500/api";
     useEffect(() => {
     getAllBlogs();
   }, []);
